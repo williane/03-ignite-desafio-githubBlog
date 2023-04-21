ba7094921875bebf371059675379a1cfd858f65c
Williane marques <williane.tads@gmail.com>
2023-04-21 10:52:30 -0300

    add feature to filter the posts

--- src/Pages/Home/Posts/index.tsx
+++ src/Pages/Home/Posts/index.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react'
-import { api } from '../../../lib/axios'
 import { Post } from './Post'
 import { Container } from './styles'
 
-interface PostsProps {
+interface PostProps {
   id: number
   number: number
   title: string
@@ -11,20 +9,11 @@ interface PostsProps {
   updated_at: string
 }
 
-export function Posts() {
-  const [posts, setPosts] = useState<PostsProps[]>([])
+interface PostsProps {
+  posts: PostProps[]
+}
 
-  async function fetchPostDataFromGithub() {
-    const response = await api.get(
-      'repos/williane/03-ignite-desafio-githubBlog/issues',
-    )
-
-    setPosts(response.data)
-  }
-  useEffect(() => {
-    fetchPostDataFromGithub()
-  }, [])
-
+export function Posts({ posts }: PostsProps) {
   return (
     <Container>
       {posts?.length > 0 &&
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,21 +1,71 @@
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 import { Posts } from './Posts'
 import { Profile } from './Profile'
 import { Container, Form, SearchFormWrapper, TitleWrapper } from './styles'
+import { api } from '../../lib/axios'
+
+interface PostProps {
+  id: number
+  number: number
+  title: string
+  body: string
+  updated_at: string
+}
 
 export function Home() {
+  const [posts, setPosts] = useState<PostProps[]>([])
+  const [filter, setFilter] = useState('')
+  const totalPosts = posts.length
+  const repository = '03-ignite-desafio-githubBlog'
+  const githubLogin = 'williane'
+
+  async function fetchPostsDataFiltered() {
+    const response = await api.get('search/issues', {
+      params: {
+        q: `${filter}repo:${githubLogin}/${repository}`,
+      },
+    })
+
+    setPosts(response.data.items)
+  }
+
+  async function fetchPostDataFromGithub() {
+    const response = await api.get(`repos/${githubLogin}/${repository}/issues`)
+
+    setPosts(response.data)
+  }
+
+  useEffect(() => {
+    fetchPostDataFromGithub()
+  }, [])
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+    fetchPostsDataFiltered()
+  }
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setFilter(event.target?.value)
+  }
+
   return (
     <Container>
       <Profile />
       <SearchFormWrapper>
         <TitleWrapper>
           <strong>Publicações</strong>
-          <span>6 publicações</span>
+          <span>{totalPosts} publicações</span>
         </TitleWrapper>
-        <Form>
-          <input type="text" placeholder="Buscar conteúdo" />
+        <Form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Buscar conteúdo"
+            onChange={handleInputChange}
+            value={filter}
+          />
         </Form>
       </SearchFormWrapper>
-      <Posts />
+      <Posts posts={posts} />
     </Container>
   )
 }
